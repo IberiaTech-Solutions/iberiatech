abf3f1b38aa6b3ea435e3ae6ed81b2b355dc29e0
Javierlozo <luisloart@gmail.com>
2025-09-24 11:54:53 -0400

    Add favicon to website layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,11 @@ export const metadata: Metadata = {
   creator: 'IberiaTech Solutions',
   publisher: 'IberiaTech Solutions',
   robots: 'index, follow',
+  icons: {
+    icon: '/favicon.svg',
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg',
+  },
   openGraph: {
     type: 'website',
     locale: 'en_US',
